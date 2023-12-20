a9f454a5bf1c190cb5633380c10ba49f930d8bcd
chandanpolai <chandanpolai26@gmail.com>
2023-12-20 10:49:11 +0530

    add comments over the code

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+// ⬇️all game sounds
 const gameStart = new Audio("song/gamestart.mp3");
 const gameOver = new Audio("song/over.wav");
 const eatSound = new Audio("song/eat.wav");
@@ -9,24 +9,28 @@ const myscore = document.querySelector('h2');
 myscore.textContent = `score ➡️${0}`;
 
 let score = 0;
+// game spped and fps control  🕹️🕹️🕹️
 let speed = 10;
 let lastpaintTime = 0;
+// snake intail position
 let snakeArr = [{ x: 10, y: 5 }];
+// food intial position
 let food = { x: 8, y: 6 };
 const highRank = document.querySelector('.rankers');
 const startbtn = document.querySelector('.start');
 const snakediv = document.querySelector(".snake-div");
 
-
+// pause and continue variable
 const pauseBtn = document.querySelector('#pause');
-
 let isPaused = false;
 
 function main(ctime) {
+  // ⬇️control the game continue and pause 
   if (!isPaused) {
     requestAnimationFrame(main);
   }
 
+  // ✂️logic for fps control
   if ((ctime - lastpaintTime) / 1000 < 1 / speed) {
     return;
   }
@@ -34,19 +38,23 @@ function main(ctime) {
   gameEngine();
 }
 
+// all the main thins in this function
 function gameEngine() {
   function isCollide(snake) {
     for (let i = 1; i < snakeArr.length; i++) {
+      // 🐍snake bump their own body
       if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) {
         return true;
       }
     }
 
+    // snake bump the wall
     if (snake[0].x >= 18 || snake[0].x <= 0 || snake[0].y >= 18 || snake[0].y <= 0) {
       return true;
     }
   }
 
+  // game over function
   if (isCollide(snakeArr)) {
     gameOver.play();
     gameOver.playbackRate=2;
@@ -58,6 +66,7 @@ function gameEngine() {
     location.reload();
   }
 
+    // 🍴snake eat function and score increase functionality and localstorage to  store user score
   if (snakeArr[0].y === food.y && snakeArr[0].x === food.x) {
     eatSound.play();
     eatSound.playbackRate=1.5;
@@ -79,7 +88,7 @@ function gameEngine() {
   snakeArr[0].y += inputDir.y;
 
   snakediv.innerHTML = "";
-
+  // 😌create snake element
   snakeArr.forEach((e, index) => {
     snakeElement = document.createElement("div");
     snakeElement.style.gridRowStart = e.y;
@@ -91,7 +100,8 @@ function gameEngine() {
     }
     snakediv.append(snakeElement);
   });
-
+  
+  // 😌create food element
   foodElement = document.createElement("div");
   foodElement.style.gridRowStart = food.y;
   foodElement.style.gridColumnStart = food.x;
@@ -101,17 +111,20 @@ function gameEngine() {
 
 window.requestAnimationFrame(main);
 
+// get the user high score from the localstorage
 highRank.addEventListener('click', () => {
   let Rank = localStorage.getItem('userscore');
   highRank.textContent = `your highscore 🕹️${Rank}`;
 });
 
+// start game click the start btn
 startbtn.addEventListener("click", (e) => {
   startbtn.style.display = 'none';
   inputDir = { x: 0, y: 1 };
   bgMusic.play();
   pauseBtn.style.display = 'block'; // Show the pause button
   
+  // only work in laptop key click🕹️🕹️
   document.addEventListener("keydown", (event) => {
     gameStart.play();
     gameStart.playbackRate=2;
